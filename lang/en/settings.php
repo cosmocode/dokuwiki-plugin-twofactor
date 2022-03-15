3574ad29fb5a43eb73d5f86d5d8693d8cff79341
Andreas Gohr <andi@splitbrain.org>
2022-03-15 15:06:14 +0100

    fixed language files for settings

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,2 +1,6 @@
 <?php
+$lang["optinout"] = 'Configure how users can chose to use two factor authentication: Opt-In, Opt-Out, or Mandatory.';
+$lang["optinout_o_optin"] = 'Opt-In';
+$lang["optinout_o_optout"] = 'Opt-Out';
+$lang["optinout_o_mandatory"] = 'Mandatory';
 
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,2 +1,6 @@
 <?php
+$lang["optinout"] = "Configurez la façon dont les utilisateurs peuvent utiliser l'authentification multi-facteurs : Opt-in, Opt-out ou obligatoire.";
+$lang["optinout_o_optin"] = 'Opt-In';
+$lang["optinout_o_optout"] = 'Opt-Out';
+$lang["optinout_o_mandatory"] = 'Mandatory';
 
