e7e81ff78a372df44f2ec6bd7331372addd84246
Andreas Gohr <andi@splitbrain.org>
2022-03-16 15:29:50 +0100

    streamline language files
    We use "two-factor" and "mechanism" as our preferred terms now.

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,19 +1,19 @@
 <?php
-$lang['btn_twofactor_profile'] = 'Two Factor Settings';
+$lang['btn_twofactor_profile'] = 'Two-Factor Settings';
 
-$lang['providers'] = 'Your configured Two Factor Providers';
+$lang['providers'] = 'Your configured Two-Factor Mechanisms';
 $lang['default'] = '(Default)';
 $lang['optout'] = 'I don\'t want to use the more secure two-factor authentication.';
-$lang['newprovider'] = 'Add Two Factor Provider';
-$lang['provider'] = 'Provider';
+$lang['newprovider'] = 'Add Two-Factor Mechanism';
+$lang['provider'] = 'Mechanism';
 $lang['btn_confirm'] = 'Confirm';
-$lang['btn_default'] = 'Make Default Provider';
-$lang['btn_remove'] = 'Remove Provider Setup';
+$lang['btn_default'] = 'Make Default Mechanism';
+$lang['btn_remove'] = 'Revoke Selected Mechanism';
 $lang['btn_setup'] = 'Setup';
 
 $lang['codefail'] = 'The entered code was incorrect or expired';
 
-$lang['menu'] = 'TwoFactor Admin';
+$lang['menu'] = 'Two-Factor Admin';
 $lang['user_id']     = 'User';
 $lang['user_name']   = 'Real Name';
 $lang['user_mail']   = 'Email';
@@ -24,5 +24,5 @@ $lang['start']  = 'start';
 $lang['prev']   = 'previous';
 $lang['next']   = 'next';
 $lang['last']   = 'last';
-$lang['js']['reset_confirm'] = 'Do you really want to reset the two factor setup of this user?';
+$lang['js']['reset_confirm'] = 'Do you really want to reset the two-factor setup of this user?';
 
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,5 +1,5 @@
 <?php
-$lang["optinout"] = 'Configure how users can chose to use two factor authentication: Opt-In, Opt-Out, or Mandatory.';
+$lang["optinout"] = 'Configure how users can chose to use two-factor authentication: Opt-In, Opt-Out, or Mandatory.';
 $lang["optinout_o_optin"] = 'Opt-In';
 $lang["optinout_o_optout"] = 'Opt-Out';
 $lang["optinout_o_mandatory"] = 'Mandatory';
