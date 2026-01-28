8eb61779d2fb87b6b21a07911ac9eb2dc3f21673
Szymon Olewniczak <szyolewn@pg.edu.pl>
2026-01-28 12:39:27 +0100

    polish translation

---
+++ lang/pl/lang.php
@@ -0,0 +1,30 @@
+<?php
+$lang['btn_twofactor_profile'] = 'Ustawienia uwierzytelniania dwuskładnikowego';
+
+$lang['providers'] = 'Twoje mechanizmy uwierzytelniania dwuskładnikowego';
+$lang['default'] = '(domyślny)';
+$lang['optout'] = 'Nie chcę używać mechanizmu uwierzytelniania dwuskładnikowego.';
+$lang['newprovider'] = 'Dodaj mechanizm uwierzytelniania dwuskładnikowego';
+$lang['provider'] = 'Mechanizm';
+$lang['btn_confirm'] = 'Potwierdź';
+$lang['btn_default'] = 'Ustaw jako mechanizm domyślny';
+$lang['btn_remove'] = 'Wyłącz zaznaczone mechanizmy';
+$lang['btn_setup'] = 'Konfiguracja';
+$lang['otp'] = 'Twój kod logowania';
+
+$lang['codefail'] = 'Podany kod jest błędny lub wygasł';
+
+$lang['menu'] = 'Two-Factor Admin';
+$lang['user_id']     = 'Użytkownik';
+$lang['user_name']   = 'Imię i nazwisko';
+$lang['user_mail']   = 'E-mail';
+$lang['action'] = 'Akcja';
+$lang['reset'] = 'Reset';
+$lang['reset_not_self'] = 'Nie możesz zresetwoać własnego konta';
+$lang['search']      = 'Szukaj';
+$lang['start']  = 'start';
+$lang['prev']   = 'poprzedni';
+$lang['next']   = 'następny';
+$lang['last']   = 'ostatni';
+$lang['js']['reset_confirm'] = 'Czy na pewno chcesz zresetować ustawienia uwierzytelniania dwuskładnikowego dla tego użytkownika?';
+
---
+++ lang/pl/settings.php
@@ -0,0 +1,8 @@
+<?php
+$lang["optinout"] = 'Skonfiguruj, w jaki sposób użytkownicy mogą wybrać korzystanie z uwierzytelniania dwuskładnikowego: dobrowolna zgoda, dobrowolna rezygnacja, wymagane.';
+$lang["optinout_o_optin"] = 'dobrowolna zgoda';
+$lang["optinout_o_optout"] = 'dobrowolna rezygnacja';
+$lang["optinout_o_mandatory"] = 'wymagane';
+
+$lang["useinternaluid"] = "Jeśli ta opcja jest wyłączona, DokuWiki nie będzie wymagać ponownej autoryzacji w przypadku zmiany adresu IP użytkownika.";
+$lang["trustedIPs"] = "Wyrażenie regularne (bez ograniczników) do porównania z adresem IP użytkownika. Jeśli użytkownik pochodzi z jednego z tych adresów IP, uwierzytelnianie dwuskładnikowe zostanie pominięte.";
